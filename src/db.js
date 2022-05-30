7b1c7c93626c437ca9a451f1a2aa83eea8a2836d
igor-primo <igorprimo62@gmail.com>
2022-05-30 19:50:06 -0300

    procfile

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-	connectionString: process.env.SIMPLE_HTTP,
+	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false,
 	}
